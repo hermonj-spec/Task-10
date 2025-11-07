aacbcd4f96e332998a37fd7abc0660ac0ae5a575
hermonj-spec <hermon.samsonj@gmail.com>
2025-11-07 11:34:06 +0300

    Commit 2
    In the new version on line 14, 18, 22 and 26, getter methods and a displayUserDetails method was added.

--- Task10/accountmanagement/src/account/User.java
+++ Task10/accountmanagement/src/account/User.java
@@ -10,4 +10,24 @@ public class User {
         Email = email;
         AccessLevel = accessLevel;
     }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public String getAccessLevel() {
+        return AccessLevel;
+    }
+
+    public void displayUserDetails() {
+        System.out.println("--- User Information ---");
+        System.out.println("Username: " + UserName);
+        System.out.println("Email: " + Email);
+        System.out.println("Access Level: " + AccessLevel);
+        System.out.println();
+    }
 }
