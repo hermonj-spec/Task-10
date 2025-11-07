5461f15aa69f973f07d42f5af36c4bea86626d80
hermonj-spec <hermon.samsonj@gmail.com>
2025-11-07 11:37:24 +0300

    Commit 2
    In the new version on line 14, 18, 22 and 26, getter methods and a displaySoftwareDisplay method was added.

--- Task10/softwarecatalog/src/catalog/Software.java
+++ Task10/softwarecatalog/src/catalog/Software.java
@@ -10,4 +10,24 @@ public class Software {
         Version = version;
         Price = price;
     }
+
+    public String getSoftwareName() {
+        return SoftwareName;
+    }
+
+    public String getVersion() {
+        return Version;
+    }
+
+    public double getPrice() {
+        return Price;
+    }
+
+    public void  displaySoftwareDisplay() {
+        System.out.println("--- Software Details ---");
+        System.out.println("Software Name: " + SoftwareName);
+        System.out.println("Version: " + Version);
+        System.out.println("Price: $" + Price);
+        System.out.println();
+    }
 }
